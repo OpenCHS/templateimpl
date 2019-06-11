7c1e5f1416e915fc17c7cf090fd84a6ca2dfb42f
sidtharthan <sidthunagarajan@gmail.com>
2019-06-11 12:58:55 +0530

    Git Init

---
+++ Makefile
@@ -0,0 +1,4 @@
+deps:; npm install; npm i -g grunt;
+-include ./node_modules/openchs-idi/Makefile
+
+#######################################
---
+++ rules.js
@@ -0,0 +1,7 @@
+const _ = require('lodash');
+
+module.exports = _.merge({},
+    require('./registration/rules/registrationFormHandler'),
+    // require('./rules2'),
+    // require('./rules3'),
+);
